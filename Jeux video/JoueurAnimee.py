e886ba39c80119c6d34ad4aa585d4628585c82f6
Mehdi <mehdidydy@hotmail.com>
2019-11-12 09:27:54 -0400

    Au propre

--- Jeux video/JoueurAnimee.py
+++ Jeux video/JoueurAnimee.py
@@ -2,169 +2,191 @@ import pygame
 from ElementGraphiqueAnimee import ElementGraphiqueAnimee
 from BalleTiree import BalleTiree
 
-class Point():
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-
-    def pos(self):
-        return (self.x,self.y)
-
 class JoueurAnimee(ElementGraphiqueAnimee):
-        def __init__(self,img,x=0,y=0):
-                ElementGraphiqueAnimee.__init__(self,img["debout"],x,y)
-                self.vies = 7
-                self.alive = True
-                self.boost = 1
-                self.cheat = False
-                self.time = 0
-                self.trigger = 0
-                self.timer = False
-                self.tps = 0
-                self.image = img
-                self.numimage = 0
-                self.direction = "debout"
-                self.last_direction = "debout"
-                self.tire = []
-                self.vitesse = 12
-                self.collision = [False, False, False, False] #[haut_droite|haut_gauche|bas_gauche|bas_droite]
+	def __init__(self, img, x = 0, y = 0, mat = [], size = 72):
+		ElementGraphiqueAnimee.__init__(self, img["debout"], x, y)
+		self.vies = 7
+		self.alive = True
+		self.boost = 1
+		self.cheat = False
+		self.time = 0
+		self.trigger = 0
+		self.timer = False
+		self.tps = 0
+		self.image = img
+		self.numimage = 0
+		self.direction = "debout"
+		self.last_direction = "debout"
+		self.tire = []
+		self.vitesse = 6
+		self.collision = [False, False, False, False] #UP, DOWN, LEFT, RIGTH
+		self.limite = [0,0,0,0,0,0,0,0]
+		self.taille_tuile = size
+		self.matrice = mat
+		self.count = 0
+
+	def setLimite(self,limite,num):
+		if(self.limite[num] == 0):
+			self.limite[num] =  limite
 
 
-        def Deplacer(self,touches,x,y,bloc):
-                haut_g = Point((self.rect.x),(self.rect.y))
-                haut_d = Point((self.rect.w + self.rect.x),(self.rect.y))
-                bas_g = Point((self.rect.x),(self.rect.y + self.rect.h))
-                bas_d = Point((self.rect.x + self.rect.w), (self.rect.y + self.rect.h))
+	def verifCollision(self):
+
+		if self.matrice[int((self.rect.x )/self.taille_tuile)+1][int(self.rect.y/self.taille_tuile)] != "0":#GAUCHE
+			self.setLimite(((int((self.rect.x )/self.taille_tuile)+1)*72 - 44),0)
+		else:
+			self.limite[0] = 0
+
+		if self.matrice[int((self.rect.x )/self.taille_tuile)][int(self.rect.y/self.taille_tuile)-1] != "0":#HAUT
+			self.setLimite((int(self.rect.y/self.taille_tuile))*72 + 4,1)
+		else:
+			self.limite[1] = 0
+
+		if self.matrice[int((self.rect.x )/self.taille_tuile)][int(self.rect.y/self.taille_tuile)+1] != "0":#BAS
+			self.setLimite((int(self.rect.y/self.taille_tuile)+1)*72 - 56 , 2)
+		else:
+			self.limite[2] = 0
+
+		if self.matrice[int((self.rect.x )/self.taille_tuile)-1][int(self.rect.y/self.taille_tuile)] != "0":#DROITE
+			self.setLimite((int((self.rect.x )/self.taille_tuile)-1)*72 + 74,3)
+		else:
+			self.limite[3] = 0
 
 
-                if touches [pygame.K_UP] :
-                    self.direction = "dos"
-                    self.last_direction = "dos_s"
-                    self.numimage += 1
-                    self.collision[0] = bloc.rect.collidepoint(haut_d.pos())
-                    self.collision[1] = bloc.rect.collidepoint(haut_g.pos())
-                    if  self.collision[0] == False and self.collision[1] == False:
-                        self.rect.y -= self.vitesse * self.boost
-                    else:
-                        self.rect.y = self.rect.y
+		if self.matrice[int((self.rect.x - 1 +self.rect.w)/self.taille_tuile)][int((self.rect.y+self.rect.h)/self.taille_tuile)-1] != "0":#DEBUGGER BAS
+			self.setLimite((int(self.rect.y/self.taille_tuile))*72 + 4,1)
+		else:
+			self.limite[1] = 0
 
-                elif touches [pygame.K_DOWN]:
-                    self.direction = "face"
-                    self.last_direction = "debout"
-                    self.numimage += 1
-                    self.collision[3] = bloc.rect.collidepoint(bas_d.pos())
-                    self.collision[2] = bloc.rect.collidepoint(bas_g.pos())
-                    if  self.collision[2] == False and self.collision[3] == False:
-                        self.rect.y += self.vitesse * self.boost
-                    else:
-                        self.rect.y = self.rect.y
+		if self.matrice[int((self.rect.x - 26 +self.rect.w)/self.taille_tuile)][int((self.rect.y+self.rect.h)/self.taille_tuile)-1] != "0":#DEBUGGER HAUT
+			self.setLimite((int(self.rect.y/self.taille_tuile))*72 + 4,1)
+		else:
+			self.limite[1] = 0
 
-                elif touches [pygame.K_RIGHT]:
-                        self.direction = "droite"
-                        self.last_direction = "droite_s"
-                        self.numimage += 1
-                        self.collision[0] = bloc.rect.collidepoint(haut_d.pos())
-                        self.collision[3] = bloc.rect.collidepoint(bas_d.pos())
-                        if self.collision[0] == False and self.collision[3] == False:
-                            self.rect.x += self.vitesse * self.boost
-                        else :
-                            self.rect.x = self.rect.x
+		if self.matrice[int((self.rect.x +self.rect.w)/self.taille_tuile)-1][int((self.rect.y +self.rect.h)/self.taille_tuile)] != "0":#DEBUGGER GAUCHE
+			self.setLimite((int((self.rect.x + 88 )/self.taille_tuile)-1)*72 + 4,3)
+		else:
+			self.limite[3] = 0
 
-                elif touches [pygame.K_LEFT]:
-                        self.direction = "gauche"
-                        self.last_direction = "gauche_s"
-                        self.numimage += 1
-                        self.collision[1] = bloc.rect.collidepoint(haut_g.pos())
-                        self.collision[2] = bloc.rect.collidepoint(bas_g.pos())
-                        if self.collision[1] == False and self.collision[2] == False :
-                            self.rect.x -= self.vitesse * self.boost
-                        else:
-                            self.rect.x = self.rect.x
-                else:
-                        self.direction = self.last_direction
+	def deplacer(self, touches, window):
+		self.verifCollision()
+		largeur, hauteur = window.get_size()
+		if self.rect.x <= 0 or self.rect.x >= largeur-self.rect.w :
+			self.deltaX = - self.deltaX
 
-        def Tire(self):
+		if self.rect.y <= 0 or self.rect.y >= hauteur-self.rect.h :
+			self.deltaY = - self.deltaY
 
-            if(self.last_direction == "dos"):
-                return 0,-10
+		if touches[pygame.K_UP]:
+			self.direction = "dos"
+			self.last_direction = "dos_s"
+			self.numimage += 1
+			if self.rect.y > self.limite[1] or self.limite[1] == 0:
+				self.rect.y -= self.vitesse * self.boost
+			else:
+				self.rect.y = self.rect.y			
 
-            elif(self.last_direction == "face" or self.last_direction == "debout"):
-                return 0,10
+		elif touches[pygame.K_DOWN]:
+			self.direction = "face"
+			self.last_direction = "face"
+			self.numimage += 1
+			if self.rect.y < self.limite[2] or self.limite[2] == 0:
+				self.rect.y += self.vitesse * self.boost
+			else:
+				self.rect.y = self.rect.y
+			
 
-            elif(self.last_direction == "droite"):
-                return 10,0
+		elif touches[pygame.K_RIGHT]:
+			self.direction = "droite"
+			self.last_direction = "droite_s"
+			self.numimage += 1
+			if self.limite[0] > self.rect.x or self.limite[0] == 0:
+				self.rect.x += self.vitesse * self.boost
+			else:
+				self.rect.x = self.rect.x
 
-            elif(self.last_direction == "gauche"):
-                return -10,0
+		elif touches[pygame.K_LEFT]:
+			self.direction = "gauche"
+			self.last_direction = "gauche_s"
+			self.numimage += 1
+			if self.limite[3] < self.rect.x or self.limite[3] == 0:
+				self.rect.x -= self.vitesse * self.boost
+			else:
+				self.limite[3] = 0
+				
+		else:
+			self.direction = self.last_direction
 
-        def setTire(self,delta=[]):
-            self.tire = delta
-            return self.tire
+	def Tire(self):
+		if(self.direction == "dos"):
+			return 0,-10
+		elif(self.direction == "face" or self.direction == "debout"):
+			return 0,10
+		elif(self.direction == "droite"):
+			return 10,0
+		elif(self.direction == "gauche"):
+			return -10,0
 
-        def afficher(self, window) :
-            if (self.fps % 1 )== 0:
-                self.numimage = (self.numimage)%len(self.image[self.direction])
-                window.blit(self.image[self.direction][self.numimage],self.rect,)
+	def setTire(self,delta=[]):
+		self.tire = delta
+		return self.tire
 
-        def recevoirDegats(self):
-                self.vies -= 1
-                if self.vies <= 0:
-                        self.alive = False
+	def afficher(self, window) :
+		if (self.fps % 1 )== 0:
+			self.numimage = (self.numimage)%len(self.image[self.direction])
+			window.blit(self.image[self.direction][self.numimage],self.rect,)
 
-        def isAlive(self):
-                return self.alive
+	def recevoirDegats(self):
+		self.vies -= 1
+		if self.vies <= 0:
+			self.alive = False
 
-        def recevoirVie(self):
-                self.vies += 1
+	def isAlive(self):
+		return self.alive
 
-        def Boost(self,tour=0):
-                if self.trigger == 1:
-                        if tour < self.time:
-                                self.boost = 2 
-                        else:
-                               self.trigger = 0
-                else:
-                        self.boost = 1
-                                         
-        def Cheat(self,tour=0):
-                if self.trigger == 2:
-                        if tour < self.time:
-                                self.boost = 2
-                                self.cheat = True
-                        else:
-                                self.boost = 1
-                                self.recevoirVie()
-                                self.cheat = False
-                                self.trigger = 0
-                        
-                        #Ne prend plus de degats et recois une vie
+	def recevoirVie(self):
+		self.vies += 1
 
-        def Frame(self,tour,window,images):
-                if self.trigger == 3:
-                        if tour < self.time:
-                                self.cheat = True
-                                if (tour%4 == 0):
-                                        self.direction = "hit"
-                                else:
-                                        self.direction = self.direction
-                        else:
-                                self.trigger = 0
-                                self.cheat = False
-                                self.recevoirDegats()
-        
-        def setTime(self,tour,time,num):
-                self.time = tour + time
-                self.trigger = num
-                return self.time
+	def Boost(self,tour=0):
+		if self.trigger == 1:
+			if tour < self.time:
+				self.boost = 2 
+			else:
+				self.trigger = 0
+		else:
+			self.boost = 1
+										 
+	def Cheat(self,tour=0):
+		if self.trigger == 2:
+			if tour < self.time:
+				self.boost = 2
+				self.cheat = True
+			else:
+				self.boost = 1
+				self.recevoirVie()
+				self.cheat = False
+				self.trigger = 0
+			#Ne prend plus de degats et recois une vie
 
-        def shoot(self, touches, event, tire, images):
-                if event.type == pygame.KEYUP and event.key == pygame.K_s:
-                        tire.append(BalleTiree(images["chomp"],self.setTire(self.Tire()),self.rect.x,self.rect.y))
-                return tire, images
-## fonction(bloc.collide(self),self.direction)
-        def colliosion(self,rep,direction):
-            if rep == True:
-                if direction == "dos":
-                    self.collision[0] = True
-                else:
-                    self.collision[0] = False
+	def Frame(self,tour,window,images):
+		if self.trigger == 3:
+			if tour < self.time:
+				self.cheat = True
+				if (tour%4 == 0):
+					self.direction = "hit"
+				else:
+					self.direction = self.direction
+			else:
+				self.trigger = 0
+				self.cheat = False
+				self.recevoirDegats()
+		
+	def setTime(self,tour,time,num):
+		self.time = tour + time
+		self.trigger = num
+		return self.time
+
+	def shoot(self, touches, event, tire, images):
+		if event.type == pygame.KEYUP and event.key == pygame.K_s:
+			tire.append(BalleTiree(images["chomp"],self.setTire(self.Tire()),self.rect.x,self.rect.y))
+		return tire, images
--- Jeux video/Niveau.py
+++ Jeux video/Niveau.py
@@ -1,43 +1,86 @@
 import pygame
-from ElementGraphiqueAnimee import ElementGraphiqueAnimee
+from pygame.locals import *
 
-class Niveau(ElementGraphiqueAnimee):
-    def __init__(self,fichier,img,x=0,y=0):
-        ElementGraphiqueAnimee.__init__(self,img,x,y)
-        self.fichier = fichier
-        self.tab = []
-        self.test = False
+cheminInfo = "niveau/level0_info.txt"
+cheminNiveau = "niveau/level0.bin"
 
-    def afficherLab(self,lab,imMur,fenetre):
-	    rect = imMur[0].get_rect()
+class Tuile():
+	"""Classe représentant une tuile, on ne définit qu'une méthode
+	pour afficher la tuile à la position demandée"""
+	def __init__(self, img, x, y):
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
 
-	    for i in range(len(lab)):
-	        for j in range(len(lab[i])):
-	            if lab[i][j]==1:
-	                fenetre.blit(imMur[0],(j*rect.w, i*rect.h))
+	def afficher(self, window):
+		window.blit(self.image, self.rect)
 
-    def createLab(self,hauteur, largeur, hautCase,largCase):
-        hCase = int(hauteur/hautCase)+1
-        lCase = int(largeur/largCase)+1
+def chargerInfoTuiles(path):
+	file = open(path, "r") #on ouvre le fichier spécifier pour chager les infos
+	data = file.read()
 
-       	#Python ouvre le fichier
-        with open(self.fichier, "r") as fichier:
-            self.tab = []
-            for ligne in fichier:
-                l = []
-                for i in ligne:
-                    if i != '\n':
-                            l.append(int(i))
-                self.tab.append(l)
-        #print(tab)
-        return self.tab
+	c = 0
+	size = -1
+	temp = []
+	tiles = []
+	nb_tiles = -1
+	flag = 0
+	l = len(data)
 
-    def editLab(self,hauteur,largeur,hautCase,largCase):
-        hCase = int(hauteur/hautCase)+1
-        lCase = int(largeur/largCase)+1
+	for i in range(l):
+		if data[i] == '\n':
+			if c == 0:
+				c += 1
+				nb_tiles = int("".join(temp))
+			elif c == 1:
+				c += 1
+				size = int("".join(temp))
+			elif c > 1:
+				tiles.append("".join(temp))
+			flag = 0
+			temp = []
+		elif flag:
+			temp.append(data[i])
 
-        with open(self.fichier,"w") as fichier:
-            for x in range(hCase):
-                    for y in range(lCase):
-                        fichier.write('0')
-                    fichier.write('\n')
+		if data[i] == ' ':
+			if c == 0:
+				flag = 1
+			elif c == 1:
+				flag = 2
+			else:
+				flag = 3
+	tiles.append("".join(temp))
+
+	file.close()
+	return (nb_tiles, size, tiles)
+
+def chargerImages(paths):
+	tuiles = []
+	print(paths)
+	for i in range(len(paths)):
+		tuiles.append(pygame.image.load(paths[i]).convert_alpha())
+	return tuiles
+
+def chargerMatrice(matW, matH, path):
+	matrice = []
+	for i in range(matW):
+		temp = []
+		for j in range(matH):
+			temp.append("0")
+		matrice.append(temp)
+	if path != "":
+		file = open(path, "r")
+		data = file.read()
+		file.close()
+		i, j, c = 0, 0, 0
+		while(c < len(data) and j < matH):
+			if data[c] != ' ':
+				if i == matW:
+					j += 1
+					i = -1
+				if data[c] != '0' and data[c] != '\n':
+					matrice[i][j] = data[c]
+				i += 1
+			c += 1
+	return matrice
