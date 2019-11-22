659d288b790b494c554f7ccc69581a27d0ad578c
shadd desous <shadddesous@gmail.com>
2019-11-21 21:28:44 -0400

    bam

--- Jeux video/Niveau.py
+++ Jeux video/Niveau.py
@@ -1,96 +1,81 @@
 import pygame
-from pygame.locals import *
+from ElementGraphiqueAnimee import ElementGraphiqueAnimee
+class Tuile(ElementGraphiqueAnimee):
+    def __init__(self,img,x=0,y=0):
+        ElementGraphiqueAnimee.__init__(self,[img],x,y)
+        self.image = img
+        self.rect = img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.plein = True
 
-cheminInfo = "niveau/level0_info.txt"
-cheminNiveau = "niveau/level0.bin"
+    def Update(self,x=0,y=0):
+        if x != self.rect.x:
+            self.rect.x = x
+            self.alive = False
+        if y != self.rect.y:
+            self.rect.y = y
+            self.alive = False
+        self.rect = self.image.get_rect()
 
-class Tuile():
-	"""Classe représentant une tuile, on ne définit qu'une méthode
-	pour afficher la tuile à la position demandée"""
-	def __init__(self, img, x, y):
-		self.image = img
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-		self.alive = True
+    def move(self,x=0,y=0,Niveau = []):
+        self.rect.x += x
+        self.rect.y += y
+        Niveau.tab[int(self.rect.y/72)][int(self.rect.x/72)] = 0
 
-	def afficher(self, window):
-		window.blit(self.image, self.rect)
+    def isAlive(self):
+        return self.alive;
 
-	def collide(self,other): 
-		if self.rect.colliderect(other.rect): 
-			self.alive = False
-			return True
-		return False
 
-	def isAlive(self):
-		return self.alive
+class Niveau(ElementGraphiqueAnimee):
+    def __init__(self,fichier,img,x=0,y=0):
+        ElementGraphiqueAnimee.__init__(self,img,x,y)
+        self.fichier = fichier
+        self.images = img
+        self.tab = []
+        self.tab_mur = []
 
-def chargerInfoTuiles(path):
-	file = open(path, "r") #on ouvre le fichier spécifier pour chager les infos
-	data = file.read()
+        self.test = False
+        self.hauteur_tuile = self.images[0].get_height()
+        self.largeur_tuile = self.images[0].get_width()
 
-	c = 0
-	size = -1
-	temp = []
-	tiles = []
-	nb_tiles = -1
-	flag = 0
-	l = len(data)
+    def afficherLab(self,imMur,fenetre):
+        rect = imMur[0].get_rect()
+        self.mur = []
+        for i in range(len(self.tab)):
+            for j in range(len(self.tab[i])):
+                t = Tuile(imMur[self.tab[i][j]],j*rect.w,i*rect.h)
+                fenetre.blit(t.image,(t.rect.x, t.rect.y))
+                if(self.tab[i][j] == 0):
+                    t.plein = False
+                if(self.tab[i][j] == 1):
+                    self.tab_mur.append(t)
 
-	for i in range(l):
-		if data[i] == '\n':
-			if c == 0:
-				c += 1
-				nb_tiles = int("".join(temp))
-			elif c == 1:
-				c += 1
-				size = int("".join(temp))
-			elif c > 1:
-				tiles.append("".join(temp))
-			flag = 0
-			temp = []
-		elif flag:
-			temp.append(data[i])
+    def Update(self,fenetre,imMur,y,x):
+        self.tab[y][x] = 0
 
-		if data[i] == ' ':
-			if c == 0:
-				flag = 1
-			elif c == 1:
-				flag = 2
-			else:
-				flag = 3
-	tiles.append("".join(temp))
+    def createLab(self,hauteur, largeur, hautCase,largCase):
 
-	file.close()
-	return (nb_tiles, size, tiles)
+        hCase = int(hauteur/hautCase)+1
+        lCase = int(largeur/largCase)+1
 
-def chargerImages(paths):
-	tuiles = []
-	print(paths)
-	for i in range(len(paths)):
-		tuiles.append(pygame.image.load(paths[i]).convert_alpha())
-	return tuiles
+       	#Python ouvre le fichier
+        with open(self.fichier, "r") as fichier:
+            self.tab = []
+            for ligne in fichier:
+                l = []
+                for i in ligne:
+                    if i != '\n':
+                            l.append(int(i))
+                self.tab.append(l)
+        return self.tab
 
-def chargerMatrice(matW, matH, path):
-	matrice = []
-	for i in range(matW):
-		temp = []
-		for j in range(matH):
-			temp.append("0")
-		matrice.append(temp)
-	if path != "":
-		file = open(path, "r")
-		data = file.read()
-		file.close()
-		i, j, c = 0, 0, 0
-		while(c < len(data) and j < matH):
-			if data[c] != ' ':
-				if i == matW:
-					j += 1
-					i = -1
-				if data[c] != '0' and data[c] != '\n':
-					matrice[i][j] = data[c]
-				i += 1
-			c += 1
-	return matrice
+    def editLab(self,hauteur,largeur,hautCase,largCase):
+        hCase = int(hauteur/hautCase)+1
+        lCase = int(largeur/largCase)+1
+        # Nommer le fichier
+        with open(nomfichier,"w") as fichier:
+            for x in range(hCase):
+                    for y in range(lCase):
+                        fichier.write('0')
+                    fichier.write('\n')
