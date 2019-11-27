c40a54943a7c15c989ed5319672c24a8ab78a725
Mehdi <mehdidydy@hotmail.com>
2019-11-26 21:22:45 -0400

    Auto stash before merge of "master" and "origin/master"

--- Group/Jeux video/GameOver.py
+++ Group/Jeux video/GameOver.py
@@ -3,11 +3,9 @@ from pygame.locals import *
 from ElementGraphique import ElementGraphique
 
 def menuGameOver(score, font, x_fen, y_fen, touches, fenetre, text, etat, continuer):
-	print("oui")
 	score.rect.x = x_fen/2-70
 	score.rect.y = y_fen/2-70
 	score.afficher(fenetre)
-	print("mais bon")
 	if touches[pygame.K_RETURN]:
 		etat = "recommencer"
 	else:
@@ -18,9 +16,7 @@ def menuGameOver(score, font, x_fen, y_fen, touches, fenetre, text, etat, contin
 			if event.type == pygame.QUIT:
 				continuer = 0
 			if event.type == KEYDOWN and event.key != K_RETURN:
-				print("bando hein")
 				text += event.unicode
-				print("binks")
 	fenetre.blit(font.render(text, True, (255, 255, 255)), (x_fen/2, y_fen/2))
 
 	return etat, text, continuer
